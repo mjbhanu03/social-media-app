3e4565a84fa02dce87fa8c821db3f27ac9973683
Mange Jay <jaymange263@gmail.com>
2024-10-27 22:30:14 +0530

    Completed

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,38 +1,103 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
 const CreatePost = () => {
+  const {addPost} = useContext(PostList)
+
+  const userIdElement = useRef()
+  const postTitleElement = useRef()
+  const postbodyElement = useRef()
+  const reactionsElement = useRef()
+  const tagsElement = useRef()
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postbody = postbodyElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+
+    addPost(userId, postTitle, postbody, reactions, tags);
+
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postbodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";
+  }
+
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+    <form className="create-post" onSubmit={handleSubmit}>
+            <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Your User Id
         </label>
         <input
-          type="email"
+          type="text"
+          ref={userIdElement}
           className="form-control"
-          id="exampleInputEmail1"
+          id="userId"
           aria-describedby="emailHelp"
+          placeholder="Your user Id"
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
+
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="title" className="form-label">
+          Post Title
         </label>
         <input
-          type="password"
+          type="text"
+          ref={postTitleElement}
           className="form-control"
-          id="exampleInputPassword1"
+          id="title"
+          aria-describedby="emailHelp"
+          placeholder="How are you feeling Today...."
         />
       </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
         </label>
+        <textarea
+          type="text"
+          className="form-control"
+          ref={postbodyElement}
+          id="title"
+          aria-describedby="emailHelp"
+          placeholder="Tell us more about it" rows = "4"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={reactionsElement}
+          id="title"
+          placeholder="How many people reacted to your post"
+        />
+      </div>
+ 
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter Tags
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          ref={tagsElement}
+          id="tags"
+          placeholder="Please enter tags using space"
+        />
       </div>
       <button type="submit" className="btn btn-primary">
-        Submit
+        Post
       </button>
     </form>
   );
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -9,6 +9,8 @@ const postListReducer = (currPostList, action) => {
   let newPostList = currPostList;
   if(action.type === 'DELETE_POST') {
     newPostList = currPostList.filter(post => (post.id !== action.payload.postId))
+  } else if (action.type === "ADD_POST"){
+    newPostList = [action.payload, ...currPostList]
   }
   return newPostList;
 };
@@ -17,7 +19,19 @@ const PostListProvider = ({ children }) => {
   const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
 
-const addPost = () => {};
+const addPost = (userId, postTitle, postbody, reactions, tags) => {
+  dispatchPostList({
+    type: 'ADD_POST',
+    payload:   {
+      id: Date.now(),
+      title: postTitle,
+      body: postbody,
+      reactions: reactions,
+      userId: userId,
+      tags:tags,
+    }
+  })
+};
 
 const deletePost = (postId) => {
   dispatchPostList({
